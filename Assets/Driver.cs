a29eba27397d3f5b1fae3fbf8eb016b30c20309c
TerminalSoul <theterminalsoul@gmail.com>
2025-10-05 18:01:45 +0530

    just cheking something again

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,12 +5,12 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(0, 0, -0.02f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, -0.02f);
+        
     }
 }
